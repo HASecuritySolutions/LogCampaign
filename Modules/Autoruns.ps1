a5fe7c1056cb44b2cf088e0bf4f0a3f512d3dd78
Justin Henderson <jhenderson@tekrefresh.com>
2022-07-01 13:37:43 -0500

    Update Autoruns.ps1

--- Modules/Autoruns.ps1
+++ Modules/Autoruns.ps1
@@ -1,4 +1,4 @@
-﻿# Runs Microsoft Sysinternals Autoruns and logs results [Supports: diff]. Has prerequisite
+# Runs Microsoft Sysinternals Autoruns and logs results [Supports: diff]. Has prerequisite
 # Prerequisite - You must have autorunsc.exe downloaded and in the binary folder
 function Campaign-Autoruns {
     if($script:Architecture -eq "AMD64"){
@@ -11,11 +11,18 @@ function Campaign-Autoruns {
         if(Test-Path -Path "$script:OutputDirectory\autoruns.csv"){
             Move-Item -Force -Path "$script:OutputDirectory\autoruns.csv" -Destination "$script:OutputDirectory\autoruns.csv.old"
             $PreviousAutoruns = Import-Csv -Path "$script:OutputDirectory\autoruns.csv.old" -Delimiter "`t"
+            $PreviousAutoruns = $PreviousAutoruns
         }
         & $path "-accepteula" "-ct" "-o" "$script:OutputDirectory\autoruns.csv" "-h" "-s" "-t"
         $CurrentAutoruns = Import-Csv -Path "$script:OutputDirectory\autoruns.csv" -Delimiter "`t"
+        $properties = $CurrentAutoruns[0].psobject.properties.name
+
         if($script:Diff){
-            $content = Compare-Object -ReferenceObject $CurrentAutoruns -DifferenceObject $PreviousAutoruns -PassThru | Where-Object { $_.SideIndicator -eq '<=' }
+            $content = Compare-Object -ReferenceObject $CurrentAutoruns -DifferenceObject $PreviousAutoruns -Property "Entry Location" -PassThru | Where-Object { $_.SideIndicator -eq '<=' }
+            if($Debug){
+                Write-Host "Debug - content difference is below:"
+                $content
+            }
         } else {
             $content = $CurrentAutoruns
         }
@@ -28,8 +35,9 @@ function Campaign-Autoruns {
                 $Log = $record | ConvertTo-Json -Compress
             }
             if($Debug){
+                Write-Host "Debug - Log generated is:"
                 $Log
-            }
+            }    
             Generate-Log "Autoruns" 0 $Log
         }
     } else {
